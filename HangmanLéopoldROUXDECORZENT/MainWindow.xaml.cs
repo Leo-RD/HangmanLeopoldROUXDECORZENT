4beacd39612dc99484d567628ae4b0f0d9d4741e
leopold rouxdecorzent <leopold.rouxdecorzent@saintmichelannecy.fr>
2024-09-30 11:40:58 +0200

    Ajoutez des fichiers projet.

---
+++ HangmanLéopoldROUXDECORZENT/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HangmanLéopoldROUXDECORZENT
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void B_A_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton A cliqué !");
+        }
+
+        private void B_B_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton B cliqué !");
+        }
+
+        private void B_C_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton C cliqué !");
+        }
+
+        private void B_D_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton D cliqué !");
+        }
+
+        private void B_E_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton E cliqué !");
+        }
+
+        private void B_F_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton F cliqué !");
+        }
+
+        private void B_G_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton G cliqué !");
+        }
+
+        private void B_H_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton H cliqué !");
+        }
+
+        private void B_I_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton I cliqué !");
+        }
+
+        private void B_J_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton J cliqué !");
+        }
+
+        private void B_K_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton K cliqué !");
+        }
+
+        private void B_L_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton L cliqué !");
+        }
+
+        private void B_M_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton M cliqué !");
+        }
+
+        private void B_N_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton N cliqué !");
+        }
+
+        private void B_O_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton O cliqué !");
+        }
+
+        private void B_P_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton P cliqué !");
+        }
+
+        private void B_Q_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton Q cliqué !");
+        }
+
+        private void B_R_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton R cliqué !");
+        }
+
+        private void B_S_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton S cliqué !");
+        }
+
+        private void B_T_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton T cliqué !");
+        }
+
+        private void B_U_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton U cliqué !");
+        }
+
+        private void B_V_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton V cliqué !");
+        }
+
+        private void B_W_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton W cliqué !");
+        }
+
+        private void B_X_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton X cliqué !");
+        }
+
+        private void B_Y_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton Y cliqué !");
+        }
+
+        private void B_Z_Click(object sender, RoutedEventArgs e)
+        {
+            // Votre logique pour le clic du bouton
+            MessageBox.Show("Bouton Z cliqué !");
+        }
+
+
+    }
+}
